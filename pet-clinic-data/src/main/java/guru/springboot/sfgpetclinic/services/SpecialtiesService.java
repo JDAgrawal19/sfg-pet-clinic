58dee49b31cfb9863335b1a31affacbb774aea5b
Jitesh Dhoot <bansal.jda@gmail.com>
2021-05-31 19:30:59 +0530

    Added Specialties Map Impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/guru/springboot/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package guru.springboot.sfgpetclinic.services;
+
+import guru.springboot.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends  CrudService<Specialty, Long >{
+}
